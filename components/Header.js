9a1908fd7e7b97c23c0d7677c9f794c9bb2777f1
RameexZaidi <rameezzaidi12@outlook.com>
2025-07-27 09:07:35 -0700

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -548,7 +548,7 @@ useEffect(() => {
   <path d="M12 6V18" stroke="#23262F" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
   <path d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z" stroke="#23262F" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
 </svg>
-                      <Link className="amount" href={'/wallet'}><span >PKR <WalletBalances/>00 </span></Link>
+                      <Link className="amount" href={'/wallet'}><span >PKR <WalletBalance/>00 </span></Link>
                     </p>
                     <p className="sear-icon d-none d-xl-inline">
                       <button
