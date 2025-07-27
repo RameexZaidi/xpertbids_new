49b0aa3fa24fc03659e0299b062e10a4999e8155
RameexZaidi <rameezzaidi12@outlook.com>
2025-07-27 08:18:38 -0700

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -198,7 +198,10 @@ useEffect(() => {
           className="search-icon-btn"
           onClick={() => setSearchOpen(true)}
         >
-          
+          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+    <path d="M9.58317 17.4998C13.9554 17.4998 17.4998 13.9554 17.4998 9.58317C17.4998 5.21092 13.9554 1.6665 9.58317 1.6665C5.21092 1.6665 1.6665 5.21092 1.6665 9.58317C1.6665 13.9554 5.21092 17.4998 9.58317 17.4998Z" stroke="#606060" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M18.3332 18.3332L16.6665 16.6665" stroke="#606060" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+  </svg>
         </button> 
         </p>
                 <NotificationDropdown />
@@ -539,14 +542,13 @@ useEffect(() => {
                   </ul>
                   <div className="registration-btns-dashboard ms-auto dashboard-menu">
                     {/* User Balance */}
-                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                    <p className="user-amount d-none d-xl-flex">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
   <path d="M8.67188 14.3298C8.67188 15.6198 9.66188 16.6598 10.8919 16.6598H13.4019C14.4719 16.6598 15.3419 15.7498 15.3419 14.6298C15.3419 13.4098 14.8119 12.9798 14.0219 12.6998L9.99187 11.2998C9.20187 11.0198 8.67188 10.5898 8.67188 9.36984C8.67188 8.24984 9.54187 7.33984 10.6119 7.33984H13.1219C14.3519 7.33984 15.3419 8.37984 15.3419 9.66984" stroke="#23262F" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
   <path d="M12 6V18" stroke="#23262F" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
   <path d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z" stroke="#23262F" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
 </svg>
-                    <p className="user-amount d-none d-xl-flex">
-                     
-                      <Link className="amount" href={'/wallet'}>PKR<span ><WalletBalance/></span></Link>
+                      <Link className="amount" href={'/wallet'}><span ><WalletBalance/></span></Link>
                     </p>
                     <p className="sear-icon d-none d-xl-inline">
                       <button
